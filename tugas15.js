c93fd392ceee424881b6308aeb1a4f0368a1e433
faldyantono <96801457+faldyantono@users.noreply.github.com>
2022-02-21 02:00:22 +0700

    Add files via upload

---
+++ tugas15.js
@@ -0,0 +1,8 @@
+var AB = 8;
+var BC = 6;
+console.log("--------------------------------")
+console.log("Ditanya panjang AC");
+console.log("--------------------------------")
+
+console.log("Panjang AC adalah: SQRT(AB^2 + BC^2).")
+var AC = console.log("Panjang AC adalah: SQRT(8^2 + 6^2) =", Math.sqrt(Math.pow(AB, 2) + Math.pow(BC, 2)));
